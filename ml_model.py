39262e71a392aad07e390f4a2ee65b528f922189
Alfonso Kim <alfonso.kimq@gmail.com>
2021-03-29 12:35:41 -0600

    descripcion de la integración del modelo

--- ml_model.py
+++ ml_model.py
@@ -18,6 +18,23 @@ def create_simple_model():
 
         Modifica esta función para integrar el modelo predictivo que quieres integrar a
         tu API REST.
+
+        Esta función crea un modelo muy simple usando los datos de Tipos de Flores. Es un
+        modelo muy simple usando el algoritmo de regresión logística. Para este ejemplo 
+        no hay ningún proceso de análisis de variables, entrenamiento o comparación de 
+        múltiples modelos, etc. Estas actividades son muy importantes y se realizan en una
+        etapa posterior a la creación del modelo, en este punto tu modelo ya está entrenado
+        y probado, listo para integrarse a un modelo predicitivo.
+
+        Una vez que el modelo se encuentra listo usamos la biblioteca pickle 
+        (https://docs.python.org/3/library/pickle.html) para "guardar" el modelo en un
+        archivo en disco duro. Este archivo contiene el estado del entrenamiento y 
+        pruebas del modelo, de esta forma se "desconecta" del entorno de laboratorio del
+        modelo para que se pueda usar en una aplicación de cómputo.
+
+        El archivo en disco duro que contiene el modelo se "recupera" en el API REST, 
+        quien es responsable de transformar la entrada del API en un formato que sea
+        entendible por el modelo.
     """
 
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -33,10 +50,12 @@ def create_simple_model():
     model = LogisticRegression(solver='liblinear', multi_class='ovr')
     model.fit(X_train, Y_train)
 
-    # ----------------------------------------------------------------------
-    # Esta línea crea un archivo "pickle" que contiene el modelo predictivo,
-    # este archivo nos va a servir para usarlo en el API REST.
-    # ----------------------------------------------------------------------
+    # Hasta este punto tu modelo debe de estar integrado y funcioando como lo esperas.
+
+    # --------------------------------------------------------------------------------
+    # Esta línea crea el archivo "pickle" que contiene el modelo predictivo, este 
+    # archivo nos va a servir para reconstruir el modelo en el API REST. 
+    # --------------------------------------------------------------------------------
     pickle.dump(model, open('simple_model.pkl','wb'))
 
 
